c01868a742a557723f2038e0fd48790e2fb6770d
Aji Darma S <73057336+socute7@users.noreply.github.com>
2024-10-04 21:05:16 +0700

    icon

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,32 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import TabBar from '../components/TabBar'
 
 const _layout = () => {
   return (
-    <Tabs/>
+    <Tabs 
+    tabBar={props=> <TabBar {...props} />}
+    >
+        <Tabs.Screen
+        name="index"
+        options={{
+            title:"Home"
+        }}
+        />
+         <Tabs.Screen
+        name="profile"
+        options={{
+            title:"Profile"
+        }}
+        />
+         <Tabs.Screen
+        name="setting"
+        options={{
+            title:"Setting"
+        }}
+        />
+    </Tabs>
   )
 }
 
---
+++ components/TabBar.jsx
@@ -0,0 +1,90 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React from 'react';
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+const TabBar = ({ state, descriptors, navigation }) => {
+  const primaryColor = '#0891b2';
+  const greyColor = '#737373';
+
+  const icons = {
+    index: (color) => <AntDesign name="home" size={26} color={color} />,
+    profile: (color) => <AntDesign name="user" size={26} color={color} />,
+    setting: (color) => <AntDesign name="setting" size={26} color={color} />,
+  };
+
+  return (
+    <View style={styles.tabbar}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        if (['_sitemap', '+not-found'].includes(route.name)) return null;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            key={route.name}
+            style={styles.tabbarItem}
+            accessibilityRole="button"
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+          >
+            {icons[route.name] ? icons[route.name](isFocused ? primaryColor : greyColor) : null}
+            <Text style={{ color: isFocused ? primaryColor : greyColor }}>
+              {label}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  tabbar: {
+    position: 'absolute',
+    bottom: 25,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    marginHorizontal: 20,
+    paddingVertical: 15,
+    borderRadius: 25,
+  },
+  tabbarItem: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default TabBar;
